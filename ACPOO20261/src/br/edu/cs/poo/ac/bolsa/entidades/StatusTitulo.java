ac7b3963979150265ea8e5c00b44c79da7b91b26
Luiz Felipe de Siqueira Gonçalves <lfsg@cesar.school>
2026-04-06 09:14:02 -0300

    Finalizando Enum statustitulo

--- ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/StatusTitulo.java
+++ ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/StatusTitulo.java
@@ -1,5 +1,24 @@
 package br.edu.cs.poo.ac.bolsa.entidades;
 
-public class StatusTitulo {
+public enum StatusTitulo {
+    
+    ATIVO(1, "Titulo ativo"),
+    CANCELADO(2, "Titulo cancelado"),
+    VENCIDO(3, "Titulo vencido");
 
+    private int codigo;
+    private String descricao;
+
+    private StatusTitulo(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
