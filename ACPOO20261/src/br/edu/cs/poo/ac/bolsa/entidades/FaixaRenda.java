a855c9d5fca8aaea0786e1be34b1aa9cb525e53d
Luiz Felipe de Siqueira Gonçalves <lfsg@cesar.school>
2026-04-06 09:13:40 -0300

    Finalizando Enum faixa de renda

--- ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/FaixaRenda.java
+++ ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/FaixaRenda.java
@@ -1,5 +1,36 @@
 package br.edu.cs.poo.ac.bolsa.entidades;
 
-public class FaixaRenda {
+public enum FaixaRenda {
 
+	REGULAR(1,"Renda regular", 10000.00, 50000.00),
+	DIFERENCIADA(2,"Renda diferenciada", 5000.01, 300000.00),
+	PREMIUM(3, "Renda premium", 300000.01, 100000000.00);
+	
+	private String descricao;
+	private double valorInicial;
+	private double valorFinal;
+	private int codigo;
+	
+	private FaixaRenda(int codigo, String descricao, double valorInicial, double valorFinal){
+		this.codigo=codigo;
+		this.descricao=descricao;
+		this.valorInicial=valorInicial;
+		this.valorFinal=valorFinal;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao(){
+		return descricao;
+	}
+	
+	public double getValorInicial() {
+		return valorInicial;
+	}
+	
+	public double getValorFinal() {
+		return valorFinal;
+	}
+	
 }
