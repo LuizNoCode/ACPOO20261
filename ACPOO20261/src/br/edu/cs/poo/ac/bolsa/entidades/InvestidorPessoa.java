4bb6b94f3413f051dbc6e1ade6a4eef06636b282
Pedro Moreira <psblm@cesar.school>
2026-04-08 09:25:03 -0300

    Iniciando a classe Investidor pessoa

--- ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/InvestidorPessoa.java
+++ ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/InvestidorPessoa.java
@@ -1,5 +1,43 @@
 package br.edu.cs.poo.ac.bolsa.entidades;
 
-public class InvestidorPessoa {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class InvestidorPessoa extends Investidor {
+    private String cpf;
+    private double renda;
+    private FaixaRenda faixaRenda;
+
+    public InvestidorPessoa(String nome, Endereco endereco, LocalDate dataNascimento, BigDecimal bonus, Contatos contatos, String cpf, double renda, FaixaRenda FaixaRenda) {
+        super(nome, endereco, dataNascimento, bonus, contatos);
+        this.cpf = cpf;
+        this.renda = renda;
+        this.faixaRenda = FaixaRenda;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public FaixaRenda getFaixaRenda() {
+        return faixaRenda;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    public void setFaixaRenda(FaixaRenda faixaRenda) {
+        this.faixaRenda = faixaRenda;
+    }
+
 
 }
