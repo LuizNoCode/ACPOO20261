bd664e6687f6a72e1a1ed91f866b6fa688272d89
Pedro Moreira <psblm@cesar.school>
2026-04-09 09:14:13 -0300

    Iniciando classe titulo

--- ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/Titulo.java
+++ ACPOO20261/src/br/edu/cs/poo/ac/bolsa/entidades/Titulo.java
@@ -1,5 +1,112 @@
 package br.edu.cs.poo.ac.bolsa.entidades;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Titulo {
+    private InvestidorPessoa investidorPessoa;
+    private InvestidorEmpresa investidorEmpresa;
+    private Ativo ativo;
+    private BigDecimal valorInvestido;
+    private BigDecimal valorAtual;
+    private BigDecimal taxaDiaria;
+    private LocalDate dataAplicacao;
+    private LocalDate dataVencimento;
+    private LocalDate dataUltimoRendimento;
+    private StatusTitulo status;
+
+
+    public Titulo(InvestidorPessoa investidorPessoa, InvestidorEmpresa investidorEmpresa, Ativo ativo, BigDecimal valorInvestido, BigDecimal valorAtual, BigDecimal taxaDiaria, LocalDate dataAplicacao, LocalDate dataVencimento, LocalDate dataUltimoRendimento, StatusTitulo status) {
+        this.investidorPessoa = investidorPessoa;
+        this.investidorEmpresa = investidorEmpresa;
+        this.ativo = ativo;
+        this.valorInvestido = valorInvestido;
+        this.valorAtual = valorAtual;
+        this.taxaDiaria = taxaDiaria;
+        this.dataAplicacao = dataAplicacao;
+        this.dataVencimento = dataVencimento;
+        this.dataUltimoRendimento = dataUltimoRendimento;
+        this.status = status;
+    }
+
+    public InvestidorPessoa getInvestidorPessoa() {
+        return investidorPessoa;
+    }
+
+    public InvestidorEmpresa getInvestidorEmpresa() {
+        return investidorEmpresa;
+    }
+
+    public Ativo getAtivo() {
+        return ativo;
+    }
+
+    public BigDecimal getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public BigDecimal getValorAtual() {
+        return valorAtual;
+    }
+
+    public BigDecimal getTaxaDiaria() {
+        return taxaDiaria;
+    }
+
+    public LocalDate getDataAplicacao() {
+        return dataAplicacao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public LocalDate getDataUltimoRendimento() {
+        return dataUltimoRendimento;
+    }
+
+    public StatusTitulo getStatus() {
+        return status;
+    }
+
+    public void setInvestidorPessoa(InvestidorPessoa investidorPessoa) {
+        this.investidorPessoa = investidorPessoa;
+    }
+
+    public void setInvestidorEmpresa(InvestidorEmpresa investidorEmpresa) {
+        this.investidorEmpresa = investidorEmpresa;
+    }
+
+    public void setAtivo(Ativo ativo) {
+        this.ativo = ativo;
+    }
+
+    public void setValorInvestido(BigDecimal valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public void setValorAtual(BigDecimal valorAtual) {
+        this.valorAtual = valorAtual;
+    }
+
+    public void setTaxaDiaria(BigDecimal taxaDiaria) {
+        this.taxaDiaria = taxaDiaria;
+    }
+
+    public void setDataAplicacao(LocalDate dataAplicacao) {
+        this.dataAplicacao = dataAplicacao;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public void setDataUltimoRendimento(LocalDate dataUltimoRendimento) {
+        this.dataUltimoRendimento = dataUltimoRendimento;
+    }
+
+    public void setStatus(StatusTitulo status) {
+        this.status = status;
+    }
 
 }
